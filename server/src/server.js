c4c423f0bec457dab5e7317bd0cfaaa507b18741
Kacper Szczudło <kacper.szczudlo@gmail.com>
2026-03-20 00:20:49 +0100

    feat: setup mongodb connection and define mongoose schemas for all modules

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 import app from './app.js';
+import { connectDB } from './database/connect.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
